8c9e2d55e9fe73c2900e9c032686d428cf3ab4f3
ericw142 <eric.h.warshawsky@gmail.com>
2024-03-05 14:50:31 -0500

    removed default css

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,16 @@
 export default function Home() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            {/* login, signup buttons */}
-            {/* username displays on the top if logged in */}
-        </main>
+        <div>
+            <div className="flex items-center justify-end p-4 z-[100] w-full absolute">
+                <button className="p-2">Login</button>
+                <button className="p-2">Signup</button>
+            </div>
+            <main className="flex min-h-screen flex-col items-center justify-between p-24">
+                {/* login, signup buttons */}
+                
+                {/* username displays on the top if logged in */}
+                My Video Games List
+            </main>
+        </div>
     );
 }
