1b9991b8a0ff1c66331d2663ddcf72f4188eb6e4
ericw142 <eric.h.warshawsky@gmail.com>
2024-03-11 16:46:49 -0400

    toggle styles

--- app/list-components/HamburgerButton.tsx
+++ app/list-components/HamburgerButton.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 
 const HamburgerButton = () => {
     const hamburger = "w-16 h-2 bg-black rounded-full before:content[''] before:absolute before:w-16 before:h-2 before:bg-black before:rounded-full before:-translate-y-4 after:content[''] after:absolute after:w-16 after:h-2 after:bg-black after:rounded-full after:translate-y-4 after:transition-all after:duration-150";
-    const toggledHamburger = "";
+    const toggledHamburger = "w-16 h-0 bg-white rounded-full before:content[''] before:absolute before:w-16 before:h-2 before:bg-black before:rounded-full before:-translate-y-0 after:content[''] after:absolute after:w-16 after:h-2 after:bg-black after:rounded-full after:translate-y-0 after:transition-all after:duration-150 before:rotate-45 after:-rotate-45";
     const [toggledClass, setToggledClass] = useState<string>(hamburger)
 
     return (
