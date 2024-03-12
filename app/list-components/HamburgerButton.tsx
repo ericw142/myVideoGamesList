23a8b696c0b36cfd4c95256c1b85d1a27cd3254e
ericw142 <eric.h.warshawsky@gmail.com>
2024-03-12 10:07:38 -0400

    updated hamburger styles

--- app/list-components/HamburgerButton.tsx
+++ app/list-components/HamburgerButton.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 
 const HamburgerButton = () => {
-    const hamburger = "w-16 h-2 bg-black rounded-full before:content[''] before:absolute before:w-16 before:h-2 before:bg-black before:rounded-full before:-translate-y-4 after:content[''] after:absolute after:w-16 after:h-2 after:bg-black after:rounded-full after:translate-y-4 after:transition-all after:duration-150";
-    const toggledHamburger = "w-16 h-0 bg-white rounded-full before:content[''] before:absolute before:w-16 before:h-2 before:bg-black before:rounded-full before:-translate-y-0 after:content[''] after:absolute after:w-16 after:h-2 after:bg-black after:rounded-full after:translate-y-0 after:transition-all after:duration-150 before:rotate-45 after:-rotate-45";
+    const hamburger = "w-8 h-1 bg-black rounded-full before:content[''] before:absolute before:w-8 before:h-1 before:bg-black before:rounded-full before:-translate-y-2 after:content[''] after:absolute after:w-8 after:h-1 after:bg-black after:rounded-full after:translate-y-2 after:transition-all after:duration-150";
+    const toggledHamburger = "w-8 h-0 bg-white rounded-full before:content[''] before:absolute before:w-8 before:h-1 before:bg-black before:rounded-full before:-translate-y-0 after:content[''] after:absolute after:w-8 after:h-1 after:bg-black after:rounded-full after:translate-y-0 after:transition-all after:duration-150 before:rotate-45 after:-rotate-45";
     const [toggledClass, setToggledClass] = useState<string>(hamburger)
 
     return (
@@ -14,7 +14,7 @@ const HamburgerButton = () => {
                     setToggledClass(toggledHamburger)
                 }
             }}
-            className="grid place-content-center w-20 h-20 p-6 mx-auto"
+            className="grid place-content-center p-6"
         >
             <div className={toggledClass}></div>
         </div>
--- app/list-components/List.tsx
+++ app/list-components/List.tsx
@@ -11,7 +11,7 @@ const List = (props: Props) => {
         <div>
             {JSON.parse(props.list).map((el: { id: string | undefined, name: string | undefined, background_image: string | undefined }, i: number) => {
                 return (
-                    <div key={`${i}-list-item-${el.id}`} className="grid grid-cols-3 gap-2">
+                    <div key={`${i}-list-item-${el.id}`} className="grid grid-cols-3 gap-2 border-b-2 border-black">
                         <div>
                             <img className='h-[100px] w-full' src={el.background_image} alt={el.name} width="auto" height="100px"/>
                         </div>
@@ -19,7 +19,9 @@ const List = (props: Props) => {
                             <p className="font-semibold">{el.name}</p>
                         </div>
                         <div>
-                            <HamburgerButton />
+                            <div className='ml-[80px] mt-[18px]'>
+                                <HamburgerButton />
+                            </div>
                         </div>
                     </div>
                 )
